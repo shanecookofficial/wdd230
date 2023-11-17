33187ccad5882656f527b8ae450dba583653072f
shanecookofficial <shanecookofficial@gmail.com>
2023-11-16 20:09:50 -0700

    Completed Web Storage Activity

--- lesson07/scripts/bom.js
+++ lesson07/scripts/bom.js
@@ -1,53 +1,60 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const input = document.querySelector('#favchap');
-    const button = document.querySelector('button');
-    const list = document.querySelector('#list');
+// References to DOM elements
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
-    let chaptersArray = getChapterList() || [];
+// Initialize chaptersArray with data from localStorage or an empty array
+let chaptersArray = getChapterList();
 
-    chaptersArray.forEach(chapter => {
-        displayList(chapter);
-    });
-
-    button.addEventListener('click', () => {
-        if (input.value.trim() !== '') {
-            displayList(input.value);
-            chaptersArray.push(input.value);
-            setChapterList(chaptersArray);
-            input.value = '';
-            input.focus();
-        } else {
-            input.focus();
-        }
-    });
-
-    function displayList(item) {
-        let li = document.createElement('li');
-        let deleteButton = document.createElement('button');
-        li.textContent = item;
-        deleteButton.textContent = '❌';
-        deleteButton.classList.add('delete');
-        li.append(deleteButton);
-        list.append(li);
-        deleteButton.addEventListener('click', function () {
-            list.removeChild(li);
-            deleteChapter(li.textContent);
-            input.focus();
-        });
-    }
-
-    function setChapterList(chapters) {
-        localStorage.setItem('myFavBOMList', JSON.stringify(chapters));
-    }
-
-    function getChapterList() {
-        const data = JSON.parse(localStorage.getItem('myFavBOMList'));
-        return data;
-    }
-
-    function deleteChapter(chapter) {
-        chapter = chapter.slice(0, chapter.length - 1);
-        chaptersArray = chaptersArray.filter(item => item !== chapter);
-        setChapterList(chaptersArray);
-    }
+// Populate the displayed list of chapters
+chaptersArray.forEach(({ name, id }) => {
+  displayList(name, id);
 });
+
+// Button click event listener
+button.addEventListener('click', () => {
+  if (input.value.trim() !== '') {
+    const id = Date.now(); // Unique identifier based on the current timestamp
+    displayList(input.value, id);
+    chaptersArray.push({ name: input.value, id: id });
+    setChapterList();
+    input.value = '';
+    input.focus();
+  }
+});
+
+// Function to display a chapter in the list
+function displayList(item, id) { // id is now a parameter
+  let li = document.createElement('li');
+  li.dataset.id = id; // Setting the data-id attribute
+  let deleteButton = document.createElement('button');
+
+  li.textContent = item;
+
+  deleteButton.textContent = '❌';
+  deleteButton.classList.add('delete');
+  li.appendChild(deleteButton);
+
+  list.appendChild(li);
+
+  deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    deleteChapter(id); // Now passing id to deleteChapter
+    input.focus();
+  });
+}
+
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  let storedChapters = localStorage.getItem('myFavBOMList');
+  return storedChapters ? JSON.parse(storedChapters) : [];
+}
+
+// Function to delete a chapter
+function deleteChapter(id) {
+  chaptersArray = chaptersArray.filter(item => item.id !== id);
+  setChapterList();
+}
