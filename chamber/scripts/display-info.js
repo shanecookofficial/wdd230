fe400e98b8d5ba2956eee7e16bbe08a2b503c8ac
shanecookofficial <shanecookofficial@gmail.com>
2023-11-25 14:43:10 -0700

    Completed 07: Chamber Discover Page

--- chamber/scripts/display-info.js
+++ chamber/scripts/display-info.js
@@ -12,3 +12,26 @@ document.querySelectorAll('.info-title').forEach(title => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const visitMessageElement = document.getElementById('visitMessage');
+    const lastVisit = localStorage.getItem('lastVisit');
+    const currentDate = new Date();
+    
+    if (lastVisit) {
+        const lastVisitDate = new Date(lastVisit);
+        const differenceInTime = currentDate - lastVisitDate;
+        const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
+
+        if (differenceInDays < 1) {
+            visitMessageElement.textContent = "Back so soon! Awesome!";
+        } else {
+            let dayText = differenceInDays === 1 ? "day" : "days";
+            visitMessageElement.textContent = `You last visited ${differenceInDays} ${dayText} ago.`;
+        }
+    } else {
+        visitMessageElement.textContent = "Welcome! Let us know if you have any questions.";
+    }
+
+    localStorage.setItem('lastVisit', currentDate.toISOString());
+});
