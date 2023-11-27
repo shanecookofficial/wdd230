9a920e535e6bc949d877d916e16676852371c2f0
shanecookofficial <shanecookofficial@gmail.com>
2023-11-26 23:05:40 -0700

    Did banner stuff

--- chamber/scripts/display-info.js
+++ chamber/scripts/display-info.js
@@ -34,6 +34,29 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     localStorage.setItem('lastVisit', currentDate.toISOString());
+
+    // Function to check if today is between Sunday and Wednesday
+    function shouldShowBanner() {
+        const today = new Date().getDay();
+        // Sunday = 0, Monday = 1, Tuesday = 2, Wednesday = 3
+        return today >= 0 && today <= 3;
+    }
+
+    // Select the meet and greet banner
+    const meetGreetBanner = document.getElementById('meet-greet-banner');
+
+    // Show or hide the banner based on the day
+    if (shouldShowBanner()) {
+        meetGreetBanner.style.display = 'block';
+    } else {
+        meetGreetBanner.style.display = 'none';
+    }
+
+    // Close button functionality
+    const closeButton = document.getElementById('banner-close-button');
+    closeButton.addEventListener('click', function() {
+        meetGreetBanner.style.display = 'none';
+    });
 });
 
 document.getElementById('dateTime').value = Date.now();
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const apiKey = "fe02b3f5d845399f215fd3243dea6312";
-    
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rexburg,US&units=imperial&appid=${apiKey}`;
 
+    // API URLs for current weather and forecast
+    const currentWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rexburg,US&units=imperial&appid=${apiKey}`;
+    const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Rexburg,US&units=imperial&appid=${apiKey}`;
+
+    // Elements for current weather display
     const weatherIconElement = document.getElementById("weather-icon");
     const currentTempElement = document.getElementById("current-temp");
     const weatherFeelElement = document.getElementById("weather-feel");
@@ -14,52 +17,65 @@ document.addEventListener("DOMContentLoaded", function () {
         return str.replace(/\b\w/g, (match) => match.toUpperCase());
     }
 
-    // Calculate the wind chill factor
+    // Function to calculate the wind chill factor
     function calculateWindChill(temperature, windSpeed) {
         if (temperature <= 50 && windSpeed > 3) {
             const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-            return Math.round(windChill); // Round wind chill to nearest whole number
+            return Math.round(windChill);
         } else {
-            return "N/A"; // Wind chill calculation not applicable
+            return "N/A";
         }
     }
 
-    // Fetch weather data from OpenWeatherMap
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            // Extract relevant weather information from the API response
-            const temperature = Math.round(data.main.temp); // Round temperature to nearest whole number
+    // Fetch and display current weather
+    fetch(currentWeatherApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const temperature = Math.round(data.main.temp);
             const weatherDescription = data.weather[0].description;
             const weatherIcon = data.weather[0].icon;
             const humidity = data.main.humidity;
-            const windSpeed = data.wind.speed; // Wind speed in mph
+            const windSpeed = data.wind.speed;
 
-            // Capitalize the first letter of each word in weather description
             const capitalizedWeatherDescription = capitalizeWords(weatherDescription);
-
-            // Update the HTML elements with weather data
             currentTempElement.textContent = `${temperature}°F`;
             weatherFeelElement.textContent = capitalizedWeatherDescription;
             humidityElement.textContent = `${humidity}%`;
 
-            // Calculate wind chill or display "N/A"
             const windChill = calculateWindChill(temperature, windSpeed);
+            windChillElement.textContent = windChill === "N/A" ? windChill : `${windChill}°F`;
 
-            if (windChill === "N/A") {
-                windChillElement.textContent = "N/A";
-            } else {
-                windChillElement.textContent = `${windChill}°F`;
-            }
-
-            // Construct the weather icon URL
             const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
-            
-            // Set the weather icon image source
             weatherIconElement.src = weatherIconUrl;
-            weatherIconElement.alt = capitalizedWeatherDescription; // Set alt text for accessibility
+            weatherIconElement.alt = capitalizedWeatherDescription;
         })
-        .catch((error) => {
-            console.error("Error fetching weather data:", error);
+        .catch(error => {
+            console.error("Error fetching current weather data:", error);
         });
+
+    // Fetch and display 3-day forecast
+    fetch(forecastApiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const forecasts = data.list;
+        let dayCount = 0;
+        let currentDate = new Date().getDate();
+
+        for (let i = 0; i < forecasts.length && dayCount < 3; i++) {
+            const forecastTime = new Date(forecasts[i].dt_txt);
+            if (forecastTime.getDate() !== currentDate) {
+                currentDate = forecastTime.getDate();
+                dayCount++;
+
+                const temperature = Math.round(forecasts[i].main.temp);
+                const description = capitalizeWords(forecasts[i].weather[0].description);
+
+                const forecastElement = document.getElementById(`forecast-day-${dayCount}`);
+                forecastElement.innerHTML = `<strong>${forecastTime.toDateString()}: </strong>${temperature}°F, ${description}`;
+            }
+        }
+    })
+    .catch(error => {
+        console.error("Error fetching forecast data:", error);
+    });
 });
