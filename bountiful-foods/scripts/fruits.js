e098008f3dd1451130680d68f9f1c180d3ec9dac
shanecookofficial <shanecookofficial@gmail.com>
2023-12-10 17:01:21 -0700

    Kept working on the order page

--- bountiful-foods/scripts/fruits.js
+++ bountiful-foods/scripts/fruits.js
@@ -1,30 +1,66 @@
-const allFruitsUrl = 'https://fruityvice.com/api/fruit/all';
-
-function populateFruits() {
-    fetch(allFruitsUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data && Array.isArray(data)) {
-                updateDropdowns(data);
-            }
-        })
-        .catch(error => console.error('Error fetching fruit data:', error));
+// Function to fetch fruits data from JSON file
+async function fetchFruitsData() {
+    try {
+        const response = await fetch('data/fruits.json'); // Corrected path
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching fruits data:', error);
+        return [];
+    }
 }
 
-function updateDropdowns(fruits) {
+
+// Function to populate dropdown options
+function populateDropdowns(fruitsData) {
     const dropdowns = document.querySelectorAll('select');
     dropdowns.forEach(dropdown => {
-        // Clear existing options
-        dropdown.innerHTML = '';
-
-        fruits.forEach(fruit => {
+        fruitsData.forEach(fruit => {
             const option = document.createElement('option');
-            option.value = fruit.name.toLowerCase();
+            option.value = fruit.name;
             option.textContent = fruit.name;
             dropdown.appendChild(option);
         });
     });
 }
 
-// Call the function to populate fruits on page load
-window.onload = populateFruits;
+// Function to calculate and update nutritional information
+function updateNutrition(fruitsData) {
+    let totalCalories = 0, totalProtein = 0, totalCarbs = 0, totalFat = 0, totalSugar = 0;
+
+    document.querySelectorAll('select').forEach(select => {
+        const selectedFruit = fruitsData.find(fruit => fruit.name === select.value);
+        if (selectedFruit) {
+            totalCalories += selectedFruit.nutritions.calories;
+            totalProtein += selectedFruit.nutritions.protein;
+            totalCarbs += selectedFruit.nutritions.carbohydrates;
+            totalFat += selectedFruit.nutritions.fat;
+            totalSugar += selectedFruit.nutritions.sugar;
+        }
+    });
+
+    document.getElementById('calories').textContent = parseFloat(totalCalories.toFixed(2));
+    document.getElementById('protein').textContent = parseFloat(totalProtein.toFixed(2));
+    document.getElementById('carbs').textContent = parseFloat(totalCarbs.toFixed(2));
+    document.getElementById('fat').textContent = parseFloat(totalFat.toFixed(2));
+    document.getElementById('sugar').textContent = parseFloat(totalSugar.toFixed(2));
+}
+
+
+// Event listener for dropdown changes
+function setupDropdownListeners(fruitsData) {
+    document.querySelectorAll('select').forEach(select => {
+        select.addEventListener('change', () => updateNutrition(fruitsData));
+    });
+}
+
+// Initialize the script
+async function init() {
+    const fruitsData = await fetchFruitsData();
+    populateDropdowns(fruitsData);
+    setupDropdownListeners(fruitsData);
+    updateNutrition(fruitsData); // To update initially with default selections
+}
+
+// Call init on window load
+window.onload = init;
